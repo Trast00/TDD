045da312a621b3070510112195df1483f767f5ed
Mukama Steven <stevenmukama1@gmail.com>
2023-03-23 15:37:39 +0200

    Add tests for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -7,4 +7,15 @@ class Solver
   def reverse(word)
     word.reverse
   end
+  def fizzbuzz(n)
+    if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+      elsif (n % 3).zero?
+      'fizz'
+      elsif (n % 5).zero?
+      'buzz'
+    else
+      n.to_s
+    end
+  end
 end
